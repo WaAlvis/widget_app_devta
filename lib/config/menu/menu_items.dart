2c3f1b0585b33bdbd5fe69a784a9be3e7ddbf316
William Andres Alvis Palencia <alvis.wap@gmail.com>
2023-10-22 07:58:39 -0500

    Tutorial page terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -27,34 +27,32 @@ const menuItems = [
     link: '/cards',
     incon: Icons.card_membership_outlined,
   ),
-   MenuItem(
+  MenuItem(
     title: 'Progres',
     subtitle: 'Indicadores de progreso',
     link: '/progress',
     incon: Icons.refresh,
   ),
-   MenuItem(
+  MenuItem(
     title: 'Snackbars y Dialogos',
     subtitle: 'Sackbars y Dialogos',
     link: '/snackbars_dialogs',
     incon: Icons.info_outline,
   ),
-  
   MenuItem(
     title: 'Animated Container',
     subtitle: 'Animacion de su forma y color',
     link: '/animated',
     incon: Icons.move_down_rounded,
   ),
-  
-   MenuItem(
-    title: 'UI Controls + Tiles', 
-    subtitle: 'Una serie de controles de Flutter', 
-    link: '/ui-controls', 
-    incon: Icons.car_rental_outlined
-  ),
-
-  
-  
-  
+  MenuItem(
+      title: 'UI Controls + Tiles',
+      subtitle: 'Una serie de controles de Flutter',
+      link: '/ui-controls',
+      incon: Icons.car_rental_outlined),
+  MenuItem(
+      title: 'Introducción a la aplicación',
+      subtitle: 'Pequeño tutorial introductorio',
+      link: '/tutorial',
+      incon: Icons.accessible_rounded),
 ];
--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,8 +1,8 @@
 export 'package:widget_app_devta/presentation/screens/animated/animated_screen.dart';
+export 'package:widget_app_devta/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widget_app_devta/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widget_app_devta/presentation/screens/cards/cards_screen.dart';
 export 'package:widget_app_devta/presentation/screens/home/home_screen.dart';
 export 'package:widget_app_devta/presentation/screens/progress/progress_screen.dart';
 export 'package:widget_app_devta/presentation/screens/snackbar/snackbar_screen.dart';
 export 'package:widget_app_devta/presentation/screens/ui_controls/ui_controls_screen.dart';
- 
